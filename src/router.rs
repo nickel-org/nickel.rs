6656f0c6dd16232dd7138cef693b6b5788e737b8
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-06-12 00:24:45 +0200

    WIP

--- src/router.rs
+++ src/router.rs
@@ -1,5 +1,7 @@
 use http::server::{Request, ResponseWriter};
 use regex::Regex;
+use collections::hashmap::HashMap;
+use std;
 
 /// A Route is the basic data structure that stores both the path
 /// and the handler that gets executed for the route.
@@ -7,14 +9,25 @@ use regex::Regex;
 struct Route {
     pub path: String,
     pub handler: fn(request: &Request, response: &mut ResponseWriter),
+    pub variables: HashMap<String, int>,
     matcher: Regex
 }
 
 impl Clone for Route {
     fn clone(&self) -> Route {
-        Route { path: self.path.clone(), handler: self.handler, matcher: self.matcher.clone() }
+        Route { 
+            path: self.path.clone(), 
+            handler: self.handler, 
+            matcher: self.matcher.clone(),
+            variables: self.variables.clone() 
+        }
     }
 }
+
+// struct RouteResult {
+//     pub route: Route,
+//     pub variables: HashMap<String, String>
+// }
 
 /// The RouteRegexFactory is responsible to convert paths to Regex patterns to
 /// match against concrete URLs
@@ -23,9 +36,9 @@ struct RouteRegexFactory;
 impl RouteRegexFactory {
     fn create_regex (route_path: &str) -> Regex {
 
-        static VALID_SEQUENCE:&'static str  = ".[a-zA-Z0-9_-]*";
-        static REGEX_START:&'static str     = "^";
-        static REGEX_END:&'static str       = "$";
+        static VARIABLE_SEQUENCE:&'static str  = "(.[a-zA-Z0-9_-]*)";
+        static REGEX_START:&'static str        = "^";
+        static REGEX_END:&'static str          = "$";
 
         // this should better be a regex! macro but I couldn't get it to work
         let regex = match Regex::new(r":[a-zA-Z0-9_-]*") {
@@ -34,13 +47,32 @@ impl RouteRegexFactory {
         };
 
         let result = REGEX_START.to_string()
-                                .append(regex.replace_all(route_path, VALID_SEQUENCE).as_slice())
+                                .append(regex.replace_all(route_path, VARIABLE_SEQUENCE).as_slice())
                                 .append(REGEX_END);
 
         match Regex::new(result.as_slice()) {
             Ok(re) => re,
             Err(err) => fail!("{}", err)
         }
+    }
+
+    fn get_variable_info<'a> (route_path: &'a str) -> HashMap<&'a str, int> {
+        // yep, that's duplicated. We'll fix that once we figured out how to use the regex macro
+        let regex = match Regex::new(r":[a-zA-Z0-9_-]*") {
+            Ok(re) => re,
+            Err(err) => fail!("{}", err)
+        };
+
+        // this is very imperative. Let's improve on that.
+        let mut map = HashMap::new();
+        let mut i = 0;
+        for matched in regex.captures_iter(route_path) {
+            std::io::stdout().write_line(matched.at(0));
+            map.insert(matched.at(0), i);
+            i = i + 1;
+        }
+
+        map
     }
 }
 
@@ -64,7 +96,8 @@ impl Router {
         let route = Route {
             path: path,
             matcher: matcher,
-            handler: handler
+            handler: handler,
+            variables: HashMap::new()
         };
         self.routes.push(route);
     }
@@ -72,13 +105,33 @@ impl Router {
     pub fn match_route<'a>(&'a self, path: String) -> Option<&'a Route> {
         self.routes.iter().find(|item| item.matcher.is_match(path.as_slice()))
     }
+
+    // pub fn match_route<'a>(&'a self, path: String) -> Option<&'a RouteResult> {
+    //     let route = self.routes.iter().find(|item| item.matcher.is_match(path.as_slice()));
+
+
+    // }
 }
 
 
 #[test]
 fn creates_valid_regex_for_var_routes () {
+    let map = RouteRegexFactory::get_variable_info("foo/:uid/bar/:groupid");
+    
+    assert_eq!(map.len(), 2);
+    assert_eq!(map.get(&":uid"), &0);
+    assert_eq!(map.get(&":groupid"), &1);
+}
+
+#[test]
+fn can_get_variable_infos () {
     let regex = RouteRegexFactory::create_regex("foo/:uid/bar/:groupid");
     assert_eq!(regex.is_match("foo/4711/bar/5490"), true);
+
+    let caps = regex.captures("foo/4711/bar/5490").unwrap();
+
+    assert_eq!(caps.at(1), "4711");
+    assert_eq!(caps.at(2), "5490");
     assert_eq!(regex.is_match("foo/"), false);
 }
 
