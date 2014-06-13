99e43d13f67247a447aac9abeef5a41fed9097d4
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-06-13 23:24:50 +0200

    refactor(router.rs): use regex! macro + some renamings

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ extern crate time;
 extern crate http;
 extern crate collections;
 extern crate regex;
-#[phase(plugin)]extern crate regex_macros;
+#[phase(syntax)]extern crate regex_macros;
 
 pub use floor::Floor;
 pub use request::Request;
