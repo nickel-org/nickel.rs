ce198d0d81b6c763aacef40e78422f7352758dab
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-06-13 22:14:36 +0200

    chore(example): fix style

--- examples/example.rs
+++ examples/example.rs
@@ -11,7 +11,7 @@ fn main() {
     // we would love to use a closure for the handler but it seems to be hard
     // to achieve with the current version of rust.
 
-    fn fooHandler (request: Request, response: &mut ResponseWriter) -> () {
+    fn user_handler (request: Request, response: &mut ResponseWriter) {
 
         let text = String::new()
                     .append("This is user: ")
@@ -20,15 +20,15 @@ fn main() {
         response.write(text.as_bytes()); 
     };
 
-    fn barHandler (request: Request, response: &mut ResponseWriter) -> () { 
+    fn bar_handler (request: Request, response: &mut ResponseWriter) { 
         response.write("This is the /bar handler".as_bytes()); 
     };
 
     // go to http://localhost:6767/user/4711 to see this route in action
-    server.get("/user/:userid", fooHandler);
+    server.get("/user/:userid", user_handler);
 
     // go to http://localhost:6767/bar to see this route in action
-    server.get("/bar", barHandler);
+    server.get("/bar", bar_handler);
 
     server.listen(6767);
 }
