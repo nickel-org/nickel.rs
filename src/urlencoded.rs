2c92a37b81d6cb71ab369f4fb3225833aa62d353
Simon Persson <simon@flaskpost.org>
2014-09-19 23:20:55 +0200

    move_iter deprecated, use into_iter instead.

--- src/urlencoded.rs
+++ src/urlencoded.rs
@@ -8,7 +8,7 @@ pub fn parse (encoded_string : &str) -> HashMap<String, Vec<String>> {
 fn create_hash_map(parsed : Vec<(String, String)>) -> HashMap<String, Vec<String>> {
     let mut map : HashMap<String, Vec<String>> = HashMap::new();
 
-    for (key, value) in parsed.move_iter() {
+    for (key, value) in parsed.into_iter() {
         map.find_with_or_insert_with(key, value,
             |_, existing, value| { existing.push(value); },
             |_, value| vec![value]
