7f1f782a86cb22b47b460809f9645df25e162ee8
Kevin Butler <haqkrs@gmail.com>
2015-03-17 22:05:50 +0000

    refactor(stability): remove regex_macro plugin use

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,8 +1,8 @@
 //!Router asigns handlers to paths and resolves them per request
 pub use self::http_router::HttpRouter;
 pub use self::router::{Router, Route, RouteResult};
-pub mod http_router;
 
+pub mod http_router;
 pub mod router;
 
 /// The path_utils collects some small helper methods that operate on the path
@@ -11,15 +11,17 @@ mod path_utils {
     use std::collections::HashMap;
 
     // matches named variables (e.g. :userid)
-    static REGEX_VAR_SEQ: Regex                 = regex!(r":([,a-zA-Z0-9_-]*)");
-    static VAR_SEQ:&'static str                 = "[,a-zA-Z0-9_-]*";
-    static VAR_SEQ_WITH_SLASH:&'static str      = "[,/a-zA-Z0-9_-]*";
-    static VAR_SEQ_WITH_CAPTURE:&'static str    = "([,a-zA-Z0-9%_-]*)";
+    lazy_static! {
+        static ref REGEX_VAR_SEQ: Regex = Regex::new(r":([,a-zA-Z0-9_-]*)").unwrap();
+    }
+    static VAR_SEQ:               &'static str = "[,a-zA-Z0-9_-]*";
+    static VAR_SEQ_WITH_SLASH:    &'static str = "[,/a-zA-Z0-9_-]*";
+    static VAR_SEQ_WITH_CAPTURE:  &'static str = "([,a-zA-Z0-9%_-]*)";
+
     // matches request params (e.g. ?foo=true&bar=false)
-    static REGEX_PARAM_SEQ:&'static str         = "(\\?[a-zA-Z0-9%_=&-]*)?";
-    static REGEX_START:&'static str             = "^";
-    static REGEX_END:&'static str               = "$";
-    pub fn create_regex (route_path: &str) -> Regex {
+    static REGEX_PARAM_SEQ: &'static str = "(\\?[a-zA-Z0-9%_=&-]*)?";
+
+    pub fn create_regex(route_path: &str) -> Regex {
         let updated_path =
             route_path.to_string()
                       // first mark all double wildcards for replacement.
@@ -33,18 +35,14 @@ mod path_utils {
                       // now replace the previously marked double wild cards (**)
                       .replace("___DOUBLE_WILDCARD___", VAR_SEQ_WITH_SLASH);
 
-        // then replace the variable symbols (:variable) with the appropriate regex
+        // then replace the variable symbols :(variable) with the appropriate regex
         let r2 = REGEX_VAR_SEQ.replace_all(&*updated_path, VAR_SEQ_WITH_CAPTURE);
-        let resultv = vec![REGEX_START,
-                           &*r2,
-                           REGEX_PARAM_SEQ,
-                           REGEX_END];
+        let line_regex = format!("^{}{}$", &*r2, REGEX_PARAM_SEQ);
 
-        let result: String = resultv.into_iter().collect();
-        Regex::new(&*result).ok().unwrap()
+        Regex::new(&line_regex).unwrap()
     }
 
-    pub fn get_variable_info (route_path: &str) -> HashMap<String, usize> {
+    pub fn get_variable_info(route_path: &str) -> HashMap<String, usize> {
         REGEX_VAR_SEQ.captures_iter(route_path)
                      .enumerate()
                      .filter_map(|(i, matched)| matched.at(1).map(|m| (m.to_string(), i)))
