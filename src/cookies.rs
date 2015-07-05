a3e3c26b39e2a4cdcb46e7bcccb4ef3ea1a8f2c3
Kevin Butler <haqkrs@gmail.com>
2015-07-05 23:45:11 +0100

    fix(cookies): add explicit implementations for Cookies trait
    The 1.2 beta has a regression for this kind of code, so being
    explicit should allow us to compile on all targets.

--- src/cookies.rs
+++ src/cookies.rs
@@ -1,5 +1,5 @@
 use {Request, Response};
-use plugin::{Plugin, Pluggable, Extensible};
+use plugin::{Plugin, Pluggable};
 use typemap::Key;
 use hyper::header;
 
@@ -8,7 +8,7 @@ use cookie::CookieJar;
 pub struct SecretKey(pub [u8; 32]);
 
 // Plugin boilerplate
-struct CookiePlugin;
+pub struct CookiePlugin;
 impl Key for CookiePlugin { type Value = CookieJar<'static>; }
 
 impl<'mw, 'conn, D> Plugin<Request<'mw, 'conn, D>> for CookiePlugin
@@ -60,21 +60,32 @@ impl<'a, D> AllowMutCookies for Response<'a, D> {}
 ///
 /// #Examples
 /// See `examples/cookies_example.rs`.
-pub trait Cookies : Pluggable + Extensible
-        where CookiePlugin: Plugin<Self, Value=CookieJar<'static>, Error=()> {
+pub trait Cookies {
     /// Provides access to an immutable CookieJar.
     ///
     /// Currently requires a mutable reciever, hopefully this can change in future.
-    fn cookies(&mut self) -> &CookieJar {
+    fn cookies(&mut self) -> &CookieJar<'static>;
+
+    /// Provides access to a mutable CookieJar.
+    fn cookies_mut(&mut self) -> &mut CookieJar<'static> where Self: AllowMutCookies;
+}
+
+impl<'mw, 'conn, D: AsRef<SecretKey>> Cookies for Request<'mw, 'conn, D> {
+    fn cookies(&mut self) -> &<CookiePlugin as Key>::Value {
         self.get_ref::<CookiePlugin>().unwrap()
     }
 
-    /// Provides access to a mutable CookieJar.
-    fn cookies_mut(&mut self) -> &mut CookieJar<'static> where Self: AllowMutCookies {
-        self.get_mut::<CookiePlugin>().unwrap()
+    fn cookies_mut(&mut self) -> &mut <CookiePlugin as Key>::Value where Self: AllowMutCookies {
+        unreachable!()
     }
 }
 
-impl<'mw, 'conn, D: AsRef<SecretKey>> Cookies for Request<'mw, 'conn, D> {}
+impl<'mw, D: AsRef<SecretKey>> Cookies for Response<'mw, D> {
+    fn cookies(&mut self) -> &<CookiePlugin as Key>::Value {
+        self.get_ref::<CookiePlugin>().unwrap()
+    }
 
-impl<'a, D: AsRef<SecretKey>> Cookies for Response<'a, D> {}
+    fn cookies_mut(&mut self) -> &mut <CookiePlugin as Key>::Value where Self: AllowMutCookies {
+        self.get_mut::<CookiePlugin>().unwrap()
+    }
+}
