86da30ef6ce8483527e79b48c58623b3721b327b
Kevin Butler <haqkrs@gmail.com>
2015-07-06 01:13:33 +0100

    chore(tests): update compile-fail tests

--- tests/compile-fail/inference_no_hints.rs
+++ tests/compile-fail/inference_no_hints.rs
@@ -9,14 +9,14 @@ fn main() {
     let mut server = Nickel::new();
 
     server.utilize(|_, res| res.send("Hello World!"));
-    //~^ ERROR type mismatch resolving `for<'r,'b,'a> <[closure tests/compile-fail
+    //~^ ERROR type mismatch resolving `for<'
     //~^^ ERROR the type of this value must be known in this context
-    //~^^^ ERROR type mismatch: the type `[closure tests/compile
+    //~^^^ ERROR type mismatch: the type `[closure@tests/compile
 
     server.get("**", |_, res| res.send("Hello World!"));
-    //~^ ERROR type mismatch resolving `for<'r,'b,'a> <[closure tests/compile-fail
+    //~^ ERROR type mismatch resolving `for<'
     //~^^ ERROR the type of this value must be known in this context
-    //~^^^ ERROR type mismatch: the type `[closure tests/compile
+    //~^^^ ERROR type mismatch: the type `[closure@tests/compile
 
     server.listen("127.0.0.1:6767");
 }
