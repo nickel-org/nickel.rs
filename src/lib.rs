2818b0e082fa5d083f6c8a753a37f47f9be4375d
Kevin Butler <haqkrs@gmail.com>
2015-08-01 16:13:50 +0100

    chore(*): remove old crate attributes
    These are no longer needed as Cargo provides that information to rustc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#![crate_name = "nickel"]
-#![crate_type = "rlib"]
-
 //!Nickel is supposed to be a simple and lightweight foundation for web applications written in Rust. Its API is inspired by the popular express framework for JavaScript.
 //!
 //!Some of the features are:
