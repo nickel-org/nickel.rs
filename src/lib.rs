294c740b5ca028c68414bda56a01bd481fccf208
Kevin Butler <haqkrs@gmail.com>
2015-08-01 16:15:13 +0100

    doc(lib): remove old overview docs
    This is out of date and the Readme/examples cover most of what is there, this
    could be rewritten in future.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-//!Nickel is supposed to be a simple and lightweight foundation for web applications written in Rust. Its API is inspired by the popular express framework for JavaScript.
-//!
-//!Some of the features are:
-//!
-//!* Easy handlers: A handler is just a function that takes a `Request` and `ResponseWriter`
-//!* Variables in routes. Just write `my/route/:someid`
-//!* Easy parameter access: `request.params.get(&"someid")`
-//!* simple wildcard routes: `/some/*/route`
-//!* double wildcard routes: `/a/**/route`
-//!* middleware
-
 extern crate time;
 extern crate rustc_serialize as serialize;
 extern crate hyper;
