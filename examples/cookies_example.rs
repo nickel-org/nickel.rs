7def1b30bfc1a3ad0eaf43b1c7f70d88af686cdc
Kevin Butler <haqkrs@gmail.com>
2015-08-27 01:20:17 +0100

    docs(cookies): add example of encrypted cookie usage

--- examples/cookies_example.rs
+++ examples/cookies_example.rs
@@ -29,5 +29,31 @@ fn main() {
         "Cookie set!"
     });
 
+    // Try `curl -c /tmp/cookie -b /tmp/cookie http://localhost:6767/secure?value=foobar`
+    // when the `secure_cookies` feature is enabled
+    // i.e. `cargo run --example cookies_example --features secure_cookies
+    if cfg!(feature = "secure_cookies") {
+        server.get("/secure", middleware! { |req, mut res|
+            let jar = res.cookies_mut()
+                         .encrypted();
+
+            let new_value = req.query().get("value")
+                                   .unwrap_or("no value")
+                                   .to_owned();
+
+            let cookie = Cookie::new("SecureCookie".to_owned(),
+                                     new_value);
+            jar.add(cookie);
+
+            // Old value from the request's Cookies
+            let old_value = req.cookies()
+                               .encrypted()
+                               .find("SecureCookie")
+                               .map(|c| c.value);
+
+            format!("Old value was {:?}", old_value)
+        });
+    }
+
     server.listen("127.0.0.1:6767");
 }
