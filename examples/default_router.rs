7348447578341696fc3042d17d0170a89517aac5
Kevin Butler <haqkrs@gmail.com>
2015-11-20 06:28:29 +0000

    test(examples): add some tests for default_router example

--- examples/default_router.rs
+++ examples/default_router.rs
@@ -10,7 +10,9 @@ fn main() {
 
     // go to http://localhost:6767/foo to see this route in action
     server.get("/:foo", middleware! { |request|
-        format!("Foo is '{}'", request.param("foo").unwrap())
+        let foo = request.param("foo").unwrap();
+        let format = request.param("format").unwrap();
+        format!("Foo is '{}'. The requested format is '{}'", foo, format)
     });
 
     server.listen("127.0.0.1:6767");
