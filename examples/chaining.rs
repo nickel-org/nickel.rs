30caf172f8e004954082e79b8d4559133218659f
Kevin Butler <haqkrs@gmail.com>
2015-11-20 22:39:49 +0000

    test(examples): add some tests for chaining example

--- examples/chaining.rs
+++ examples/chaining.rs
@@ -6,13 +6,12 @@ use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
-    server
-        .add_route(Method::Get, "/", middleware!("Hello World"))
-        .get("/get", middleware!("get"))
-        .post("/post", middleware!("post"))
-        .put("/put", middleware!("put"))
-        .patch("/patch", middleware!("patch"))
-        .delete("/delete", middleware!("delete"));
+    server.add_route(Method::Get, "/", middleware!("Hello World"))
+          .get("/get", middleware!("get"))
+          .post("/post", middleware!("post"))
+          .put("/put", middleware!("put"))
+          .patch("/patch", middleware!("patch"))
+          .delete("/delete", middleware!("delete"));
 
     server.listen("127.0.0.1:6767");
 }
