fa0511e0302ce6ce1f09323af87a3e4b79e97056
Jan-Erik Rediger <janerik@fnordig.de>
2016-02-10 21:49:24 +0100

    doc(examples): Point to correct crate

--- examples/template.rs
+++ examples/template.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut server = Nickel::new();
 
     // * NOTE *
-    // This example is deprecated, you should use nickel-mustache from crates.io
+    // This example is deprecated, you should use nickel_mustache from crates.io
     server.get("/", middleware! { |_, res|
         let mut data = HashMap::<&str, &str>::new();
         data.insert("name", "user");
