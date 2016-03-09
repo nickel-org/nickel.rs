535ebfb4a9c2d8596372488b41d6d0462fea8882
Vaelden <maugan.villatel@gmail.com>
2016-03-09 21:46:10 +0000

    feat(https): Added an HTTPS example
    Pull request: #316
    Approved by: Ryman

---
+++ examples/https.rs
@@ -0,0 +1,20 @@
+#[macro_use] extern crate nickel;
+extern crate hyper;
+
+#[cfg(feature = "ssl")]
+fn main() {
+    use hyper::net::Openssl;
+
+    use nickel::{Nickel, HttpRouter};
+
+    let ssl = Openssl::with_cert_and_key("examples/assets/self_signed.crt", "examples/assets/key.pem").unwrap();
+    let mut server = Nickel::new();
+    server.get("**", middleware!("Hello World from HTTPS"));
+    server.listen_https("127.0.0.1:6767", ssl);
+}
+
+#[cfg(not(feature = "ssl"))]
+fn main() {
+    println!("Please run this example with the feature \"ssl\" enabled\n\
+             $ cargo run --example https --features ssl");
+}
