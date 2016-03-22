7e4f49252ac4451aee6380ef210505cbe838409b
Austin Bonander <austin.bonander@gmail.com>
2016-03-21 18:06:54 -0700

    feat(*): bump hyper, mime and url dependency versions
    BREAKING CHANGE: More recent versions of the `hyper`, `mime` and `url` dependencies
    are now required.

--- src/server.rs
+++ src/server.rs
@@ -45,9 +45,7 @@ impl<D: Sync + Send + 'static> Server<D> {
         let arc = ArcServer(Arc::new(self));
         let mut server = try!(HyperServer::http(addr));
 
-        if let Some(timeout) = keep_alive_timeout {
-            server.keep_alive(timeout);
-        }
+        server.keep_alive(keep_alive_timeout);
 
         server.handle(arc)
     }
