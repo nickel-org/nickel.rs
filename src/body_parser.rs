0982ed5f906c09e8d98bd4eccb103305728b7473
Jeff Olhoeft <jolhoeft@gmail.com>
2017-11-07 15:41:34 -0800

    Change body_parser to use body_transformer, and deprecate

--- src/body_parser.rs
+++ src/body_parser.rs
@@ -1,3 +1,4 @@
+use body_transformer::{BodyError, BodyTransformer};
 use futures::Future;
 use futures::stream::Stream;
 use hyper::Chunk;
@@ -8,51 +9,27 @@ use serialize::{Decodable, json};
 use request::Request;
 use plugin::{Plugin, Pluggable};
 use status::StatusCode;
-use std::error::Error as StdError;
-use std::fmt;
-use std::io::{self, ErrorKind, Read};
-use std::str::{from_utf8, Utf8Error};
+use std::io::{self, ErrorKind};
 use typemap::Key;
 use urlencoded::{self, Params};
-
-type BodyFuture = Box<Future<Item = String, Error = Utf8Error>>;
 
 struct BodyReader;
 
 impl Key for BodyReader {
-    type Value = BodyFuture;
+    type Value = String;
 }
 
-impl<'mw, B: Stream<Item = Chunk, Error = HyperError>, D> Plugin<Request<'mw, B, D>> for BodyReader {
-    type Error = HyperError;
+impl<'mw, B: Stream<Item=Chunk, Error=HyperError>, D> Plugin<Request<'mw, B, D>> for BodyReader {
+    type Error = io::Error;
 
-    // This doesn't quite work. The Key trait requires that BodyFuture
-    // have a 'static lifetime, but we can only garantee 'mw. In
-    // practice this shouldn't be an issue, but I don't see a way to
-    // convince the compiler of it, at least in safe rust.
-    //
-    // Two thoughts:
-    //
-    //  1. Can we completely remove the 'mw lifetime at this point? would that help?
-    //  2. Give up on the plugin approach, and make StringBody trait
-    //     on Request that returns a BodyFuture, similar to how
-    //     JsonBody is managed.
-    //
-    // Current plan:
-    //
-    //  1. Implement BodyFuture, ParamFuture, and JsonFuture to
-    //     provide futures returning those values. This will be the
-    //     new interface.
-    //  2. Implement the old plugins using the new *Futures and
-    //     wait. This will be very slow and possibly problematic, so
-    //     they will be immediately deprecated.
-    fn eval(req: &mut Request<'mw, B, D>) -> Result<Box<Future<Item = String, Error = Utf8Error> + 'static>, HyperError> {
-        req.origin.body_ref().ok_or(HyperError::Incomplete).
-            map(|b| {
-                b.concat2().and_then(|body| {
-                    from_utf8(&body).map(|s| s.to_string())
-                })
-            }).map(|bs| Box::new(bs))
+    fn eval(req: &mut Request<'mw, B, D>) -> Result<String, io::Error> {
+        match req.string_future() {
+            Ok(f) => f.wait(). // sychronizes the async code with serious performance impact
+                map_err(|e| io::Error::new(ErrorKind::Other, format!("Hyper Error: {:?}", e))).
+                and_then(|r| r.map_err(|_| io::Error::new(ErrorKind::InvalidData, "Body not Utf8"))),
+            Err(BodyError::MissingBody) => Err(io::Error::new(ErrorKind::NotFound, "Body Missing")),
+            Err(e) => Err(io::Error::new(ErrorKind::Other, format!("Unexpected Error: {:?}", e))), // shouldn't happen
+        }
     }
 }
 
@@ -62,7 +39,7 @@ impl Key for FormBodyParser {
     type Value = Params;
 }
 
-impl<'mw, B: Stream<Item = Chunk, Error = HyperError>, D> Plugin<Request<'mw, B, D>> for FormBodyParser {
+impl<'mw, B: Stream<Item=Chunk, Error=HyperError>, D> Plugin<Request<'mw, B, D>> for FormBodyParser {
     type Error = BodyError;
 
     fn eval(req: &mut Request<B, D>) -> Result<Params, BodyError> {
@@ -76,6 +53,7 @@ impl<'mw, B: Stream<Item = Chunk, Error = HyperError>, D> Plugin<Request<'mw, B,
     }
 }
 
+#[deprecated(since = "0.11.0", note="Synchronizes async code with performance impact, will be removed in 0.12")]
 pub trait FormBody {
     /// Extracts URL encoded data from the request body.
     /// # Examples
@@ -94,24 +72,27 @@ pub trait FormBody {
     fn form_body(&mut self) -> Result<&Params, (StatusCode, BodyError)>;
 }
 
-impl<'mw, B: Stream<Item = Chunk, Error = HyperError>, D> FormBody for Request<'mw, B, D> {
+#[deprecated(since = "0.11.0", note="Synchronizes async code with performance impact, will be removed in 0.12")]
+impl<'mw, B: Stream<Item=Chunk, Error=HyperError>, D> FormBody for Request<'mw, B, D> {
     fn form_body(&mut self) -> Result<&Params, (StatusCode, BodyError)> {
         self.get_ref::<FormBodyParser>().map_err(|e| (StatusCode::BadRequest, e))
     }
 }
 
+#[deprecated(since = "0.11.0", note="Synchronizes async code with performance impact, will be removed in 0.12")]
 pub trait JsonBody {
-    fn json_as<T: Decodable>(&mut self) -> Result<T, HyperError>;
+    fn json_as<T: Decodable>(&mut self) -> Result<T, io::Error>;
 }
 
-impl<'mw, B: Stream<Item = Chunk, Error = HyperError>, D> JsonBody for Request<'mw, B, D> {
+#[deprecated(since = "0.11.0", note="Synchronizes async code with performance impact, will be removed in 0.12")]
+impl<'mw, B: Stream<Item=Chunk, Error=HyperError>, D> JsonBody for Request<'mw, B, D> {
     // FIXME: Update the error type.
     // Would be good to capture parsing error rather than a generic io::Error.
     // FIXME: Do the content-type check
-    fn json_as<T: Decodable>(&mut self) -> Result<T, HyperError> {
+    fn json_as<T: Decodable>(&mut self) -> Result<T, io::Error> {
         self.get_ref::<BodyReader>().and_then(|body|
             json::decode::<T>(&*body).map_err(|err|
-                HyperError::Io(io::Error::new(ErrorKind::Other, format!("Parse error: {}", err)))
+                io::Error::new(ErrorKind::Other, format!("Parse error: {}", err))
             )
         )
     }
