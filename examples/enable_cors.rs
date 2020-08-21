17805d3362fbd5fec575a93fd5655d6c86904657
Jeff Olhoeft <jolhoeft@gmail.com>
2020-08-21 00:19:08 -0700

    Update the enable_cors example

--- examples/enable_cors.rs
+++ examples/enable_cors.rs
@@ -1,29 +1,24 @@
 #[macro_use] extern crate nickel;
 
 use nickel::{Nickel, HttpRouter, Request, Response, MiddlewareResult};
-use hyper::header::{AccessControlAllowOrigin, AccessControlAllowHeaders};
+use hyper::header::{self, HeaderValue};
 
-fn enable_cors<'mw>(_req: &mut Request<'_, '_>, mut res: Response<'mw>) -> MiddlewareResult<'mw> {
+fn enable_cors(_req: &mut Request, mut res: Response) -> MiddlewareResult {
     // Set appropriate headers
-    res.set(AccessControlAllowOrigin::Any);
-    res.set(AccessControlAllowHeaders(vec![
-        // Hyper uses the `unicase::Unicase` type to ensure comparisons are done
-        // case-insensitively. Here, we use `into()` to convert to one from a `&str`
-        // so that we don't have to import the type ourselves.
-        "Origin".into(),
-        "X-Requested-With".into(),
-        "Content-Type".into(),
-        "Accept".into(),
-    ]));
+    res.set_header(header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                   HeaderValue::from_static("Any"));
+    res.set_header(header::ACCESS_CONTROL_ALLOW_HEADERS,
+                   HeaderValue::from_static("Origin, X-Requested-With, Content-Type, Accept"));
 
     // Pass control to the next middleware
     res.next_middleware()
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut server = Nickel::new();
     server.utilize(enable_cors);
     server.options("**", middleware!(""));
     server.get("**", middleware!("Hello CORS Enabled World"));
-    server.listen("127.0.0.1:6767").unwrap();
+    server.listen("127.0.0.1:6767").await.unwrap();
 }
