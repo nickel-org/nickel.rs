9b302873062f565bd6f6cb80afa3f04e05e8f380
Jeff Olhoeft <jolhoeft@gmail.com>
2020-08-22 16:17:32 -0700

    raw, string, and json bosy support

--- src/request.rs
+++ src/request.rs
@@ -1,7 +1,11 @@
 use crate::router::RouteResult;
 use plugin::{Extensible, Pluggable};
-use typemap::{ShareMap, TypeMap};
-use hyper::{Body, Request as HyperRequest};
+use typemap::{Key, ShareMap, TypeMap};
+use hyper::{Body, Request as HyperRequest, StatusCode};
+use hyper::body::{self, Bytes};
+use serde::Deserialize;
+use serde_json;
+use std::mem;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
@@ -15,6 +19,7 @@ use std::sync::Arc;
 pub struct Request<D = ()> {
     ///the original `hyper::server::Request`
     pub origin: HyperRequest<Body>,
+    body_taken: bool,
     ///a `HashMap<String, String>` holding all params with names and values
     pub route_result: Option<RouteResult>,
 
@@ -31,6 +36,7 @@ impl<D> Request<D> {
                          data: Arc<D>) -> Request<D> {
         Request {
             origin: req,
+            body_taken: false,
             route_result: None,
             map: TypeMap::custom(),
             data: data,
@@ -64,6 +70,21 @@ impl<D> Request<D> {
     pub fn extensions_mut(&mut self) -> &mut ShareMap {
         &mut self.map
     }
+
+    /// Take the body from the hyper request. Once taken the body is not longer
+    /// available. This method will return `None` in that case.
+    pub fn take_body(&mut self) -> Option<Body> {
+        if self.body_taken {
+            None
+        } else {
+            let stub = HyperRequest::new(Body::empty());
+            let origin = mem::replace(&mut self.origin, stub);
+            let (parts, body) = origin.into_parts();
+            mem::replace(&mut self.origin, HyperRequest::from_parts(parts, Body::empty()));
+            self.body_taken = true;
+            Some(body)
+        }
+    }
 }
 
 // TODO: migration cleanup - Extensible does not support ShareMap, but TypeMap is not Sync+Send
@@ -78,3 +99,41 @@ impl<D> Request<D> {
 // }
 
 // impl<D> Pluggable for Request<D> {}
+
+// Various body parsers. These used to live in the body_parser module by
+// implementing a trait on the Request struct. Async and traits are currently
+// kind of cumbersome. We use async_trait for Middleware, because users of
+// nickel need to implement Middleware. Since that is not needed for body
+// parsers, we will stick with something that will keep the interface simpler.
+
+struct RawBodyCache;
+
+impl Key for RawBodyCache {
+    type Value = Bytes;
+}
+
+impl<D> Request<D> {
+    pub async fn raw_body(&mut self) -> Result<&[u8], (StatusCode, String)> {
+        if !self.map.contains::<RawBodyCache>() {
+            // read and insert into cache
+            let body = self.take_body().
+                ok_or((StatusCode::INTERNAL_SERVER_ERROR, "body already taken".to_string()))?;
+            let bytes = body::to_bytes::<Body>(body).await.
+                map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+            self.map.insert::<RawBodyCache>(bytes);
+        }
+        // we've garanteed this unwrap is safe above
+        Ok(self.map.get::<RawBodyCache>().unwrap())
+    }
+
+    pub async fn string_body(&mut self) -> Result<String, (StatusCode, String)> {
+        let bytes = self.raw_body().await?;
+        String::from_utf8(bytes.to_vec()).map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))
+    }
+
+    pub async fn json_as<'a, T: Deserialize<'a>>(&'a mut self) -> Result<T, (StatusCode, String)> {
+        let bytes = self.raw_body().await?;
+        serde_json::from_slice::<T>(bytes).
+            map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))
+    }
+}
